52ba8e7f50c1c21fec3230120c6ac815cbb429bc
ojingwaDaniel <ojingwadanny@gmail.com>
2023-12-30 00:14:31 +0100

    Imperative Coding

--- PracticeNote.jsx
+++ PracticeNote.jsx
@@ -12,10 +12,50 @@
 //     <MainContent/>
 
 // </div>,document.getElementById('root'))
+
+
+
+
+// Imperative Coding//
     
 const h1 = document.createElement('h1')
 const div = document.getElementById('root')
 h1.textContent= 'This is an imperative way of coding '
 text.className('header')
 div.appendChild(text)
+
+
+const NavBar = (
+  <nav>
+    <h1>My Website</h1>
+    <ul>
+      <li>PRicing</li>
+      <li>About</li>
+      <li>COntact</li>
+    </ul>
+  </nav>
+);
+RenderDom.render(NavBar,document.getElementById('root'))
+///  Challenge 2
+const StaticPage  (
+    <div>
+        <nav>
+            <h1>My Website</h1>
+            <ul>
+                <li>PRicing</li>
+                <li>About</li>
+                <li>COntact</li>
+            </ul>
+        </nav>
+        <header>
+            React a Frontend FrameWork
+        </header>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+        <footer>
+            &copy; CopyRight 2023
+        </footer>
+
+    </div>
+
+)
 
