6b6e8c1ed5633f68a64a622364f3dc5f0c706c77
ojingwaDaniel <ojingwadanny@gmail.com>
2023-12-29 19:34:21 +0100

    practiced basic of  react in a simple form

---
+++ PracticeNote.jsx
@@ -0,0 +1,21 @@
+// function MainContent(){
+//     return(
+//         <div>
+//         <h1>hey im learning React</h1>
+
+//         </div>
+        
+//     )
+//     }
+// ReactDom.render(
+// <div>
+//     <MainContent/>
+
+// </div>,document.getElementById('root'))
+    
+const h1 = document.createElement('h1')
+const div = document.getElementById('root')
+h1.textContent= 'This is an imperative way of coding '
+text.className('header')
+div.appendChild(text)
+
